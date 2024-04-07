c5f0a207b1bfaec7e25c580fc6add231db997f03
Tobias Herrmann <72389927+tohe3007@users.noreply.github.com>
2024-04-07 16:52:48 +0200

    Update selector.tsx

--- lib/selector.tsx
+++ lib/selector.tsx
@@ -120,57 +120,58 @@ export default function CountrySelector({
                 }
               >
                 {COUNTRIES.filter((country) =>
-                  country.title.toLowerCase().startsWith(query.toLowerCase())
-                ).length === 0 ? (
-                  <li className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9">
-                    No countries found
-                  </li>
-                ) : (
-                  COUNTRIES.filter((country) =>
-                    country.title.toLowerCase().startsWith(query.toLowerCase())
-                  ).map((value, index) => {
-                    return (
-                      <li
-                        key={`${id}-${index}`}
-                        className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9 flex items-center hover:bg-gray-50 transition"
-                        id="listbox-option-0"
-                        role="option"
-                        onClick={() => {
-                          onChange(value.value);
-                          setQuery("");
-                          onToggle();
-                        }}
-                      >
-                        <img
-                          alt={`${value.value}`}
-                          src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${value.value}.svg`}
-                          className={"inline mr-2 h-4 rounded-sm"}
-                        />
+  country.title.toLowerCase().includes(query.toLowerCase())
+).length === 0 ? (
+  <li className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9">
+    No countries found
+  </li>
+) : (
+  COUNTRIES.filter((country) =>
+    country.title.toLowerCase().includes(query.toLowerCase())
+  ).map((value, index) => {
+    return (
+      <li
+        key={`${id}-${index}`}
+        className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9 flex items-center hover:bg-gray-50 transition"
+        id="listbox-option-0"
+        role="option"
+        onClick={() => {
+          onChange(value.value);
+          setQuery("");
+          onToggle();
+        }}
+      >
+        <img
+          alt={`${value.value}`}
+          src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${value.value}.svg`}
+          className={"inline mr-2 h-4 rounded-sm"}
+        />
 
-                        <span className="font-normal truncate">
-                          {value.title}
-                        </span>
-                        {value.value === selectedValue.value ? (
-                          <span className="text-blue-600 absolute inset-y-0 right-0 flex items-center pr-8">
-                            <svg
-                              className="h-5 w-5"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              aria-hidden="true"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </span>
-                        ) : null}
-                      </li>
-                    );
-                  })
-                )}
+        <span className="font-normal truncate">
+          {value.title}
+        </span>
+        {value.value === selectedValue.value ? (
+          <span className="text-blue-600 absolute inset-y-0 right-0 flex items-center pr-8">
+            <svg
+              className="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+        ) : null}
+      </li>
+    );
+  })
+)}
+
               </div>
             </motion.ul>
           )}
